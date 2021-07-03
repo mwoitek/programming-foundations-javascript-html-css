f75c4e61d25fd778bcd7c88cd0ef96a3e3d0516c
Marcio Woitek <woitek@usp.br>
2021-07-03 11:12:04 -0300

    Week 3 - Change Pages Interactively

---
+++ week_3/try_its/change_pages_interactively/script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    var firstDiv = document.getElementById("first_div");
+    var secondDiv = document.getElementById("second_div");
+    firstDiv.className = "dark_sea_green";
+    secondDiv.className = "english_lavender";
+}
+
+function changeText() {
+    var firstDiv = document.getElementById("first_div");
+    var secondDiv = document.getElementById("second_div");
+    firstDiv.innerHTML = "Primeiro div";
+    secondDiv.innerHTML = "Segundo div";
+}
